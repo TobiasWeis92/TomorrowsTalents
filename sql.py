a6fc4e425e0768fd3d42edc3a3372ac0f7758f43
Tobias Weis <94526149+TobiasWeis92@users.noreply.github.com>
2022-02-25 18:10:40 +0100

    Update sql.py
    Adjusted the file so that it also includes the get_data() function and removed the user, password, etc.

--- sql.py
+++ sql.py
@@ -1,16 +1,39 @@
 # Fill in credentials
-host='data-analytics-course.c8g8r1deus2v.eu-central-1.rds.amazonaws.com'
-port='5432'
-database='postgres'
-user='maximilianpiepelow'
-password='wPdcAV7K3LZxbqSn'
+host=''
+port=''
+database=''
+user=''
+password=''
 
-
+# sqlalchemy engine for writing data to a database
 from sqlalchemy import create_engine    
 engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}')
 
-# Import the Python packages for get_data() function
-
+import psycopg2 as pc
+import pandas as pd
 
 
 # Insert the get_data() function definition below
+def get_data(query):
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # create a connection to the PostgreSQL server
+        conn = pc.connect(host=host,
+                        port=port,
+                        database=database,
+                        user=user,
+                        password=password)
+		
+        fetch = pd.read_sql_query(query, conn)
+       
+	    # close the connection to the PostgreSQL database
+        conn.close()
+
+    # the code below makes the function more robust
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return(fetch);
